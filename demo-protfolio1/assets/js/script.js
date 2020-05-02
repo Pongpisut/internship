a142c5d68c7a7b21aea60790ed5e16ff615e0fd8
Pongpisut <pongpisut1997@gmail.com>
2020-05-02 16:24:57 +0700

    Add files via upload

---
+++ demo-protfolio1/assets/js/script.js
@@ -0,0 +1,29 @@
+$(window).scroll(function () {
+  if ($(this).scrollTop()) {
+    $("#toTop").fadeIn();
+  } else {
+    $("#toTop").fadeOut();
+  }
+});
+$("#toTop").click(function () {
+  //1 second of animation time
+  //html works for FFX but not Chrome
+  //body works for Chrome but not FFX
+  //This strange selector seems to work universally
+  $("html, body").animate(
+    {
+      scrollTop: 0,
+    },
+    1000
+  );
+});
+
+function Showmenu(x) {
+  x.classList.toggle("changeclass");
+}
+
+$(document).ready(function () {
+  $(".toggles").on("click", function () {
+    $(".nav-sub ul").toggleClass("open");
+  });
+});
